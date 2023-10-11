fcef68c4cb918d7fa357a9b9b2bb0a1456907c25
Anitha <anitha@iweensoft.com>
2023-10-11 22:07:31 +0530

    update

--- socket/socket.js
+++ socket/socket.js
@@ -8,10 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:3000",
-      "https://shimmering-kataifi-935423.netlify.app",
-    ],
+    origin: "*",
     // http://localhost:3000 https://shimmering-kataifi-935423.netlify.app
     methods: ["GET", "POST"],
   },
